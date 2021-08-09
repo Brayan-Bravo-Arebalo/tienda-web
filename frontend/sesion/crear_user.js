0b59a6bd6d674328b7f5fe9600ce0cbc503a596d
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-08-09 16:04:41 -0400

    agregar funcion que detecta lo escrito para validacion y agregar pagina para agregar productos a la base de datos

--- frontend/sesion/crear_user.js
+++ frontend/sesion/crear_user.js
@@ -1,3 +1,6 @@
+const validacionemail = /[a-zA-Z0-9.]{2,15}@[a-zA-Z0-9]{2,15}.[a-zA-Z]{2,5}/
+
+
 const crear = document.getElementById("new_user")
 
 crear.addEventListener("click", (e) =>{
@@ -20,4 +23,42 @@ crear.addEventListener("click", (e) =>{
     }
     
     enviarform()
+})
+
+const formulario = document.getElementById("form_crear_user")
+
+formulario.addEventListener("keyup",(e)=>{
+
+    if (e.target.classList == "nombre") {
+        if (e.target.value == "hola") {
+            
+        } else {
+            console.log(e.target.value);
+        }
+    }
+
+    if (e.target.classList == "run") {
+        console.log("usted esta escribiendo su run")
+    }
+    if (e.target.classList == "direcion") {
+        console.log("usted esta escribiendo direccion ")
+    }
+
+    if (e.target.classList == "telefono") {
+        console.log("usted esta escribiendo")
+    }
+    
+    if (e.target.classList == "email") {
+        if (e.target.value.match(validacionemail)) {
+            console.log("el correo esta escrito de forma correcta");
+        } else {
+            console.log("el correo no es valido");
+        }
+    }
+
+    if (e.target.classList == "password") {
+        console.log("usted esta escribiendo su password")
+    }
+
+
 })
