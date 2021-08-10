526b9bceee7d96954bc36b9f2ca72a824c5b2e24
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-08-10 17:23:08 -0400

    crear archivo principal para pagina web en github

---
+++ frontend/agregar producto/app.js
@@ -0,0 +1,7 @@
+const template = document.getElementById("template-img").content
+
+const fragment = document.createDocumentFragment();
+
+const boton = document.getElementById("subir-archivo");
+
+
